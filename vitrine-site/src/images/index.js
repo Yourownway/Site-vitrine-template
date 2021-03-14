8003aeeef8ca6cd0e5709dfc54bbff746ceab2d4
Chuck <lorris.dhedin@hotmail.fr>
2021-03-14 20:21:00 +0100

    up

--- vitrine-site/src/images/index.js
+++
@@ -1,4 +0,0 @@
-export { default as instagram } from "./instagram.png"
-export { default as facebook } from "./facebook.png"
-export { default as border_menu } from "./menu-border.png"
-export { default as author } from "./author.png"
